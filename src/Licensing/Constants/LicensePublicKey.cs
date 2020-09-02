741c8b91b01a2e2ed073833b6cfabb8dcb1938e7
Ahira <justiceahira@gmail.com>
2020-09-02 12:08:26 +0100

    Add static class to hold public key constants

---
+++ src/Licensing/Constants/LicensePublicKey.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using Licensing.Exceptions;
+
+namespace Licensing.Constants {
+    public static class LicensePublicKey {
+        private static string _keyString;
+
+        public static string KeyString {
+            get {
+                if (string.IsNullOrEmpty(_keyString))
+                    throw new LicensePublicKeyNotDefinedException();
+
+                return _keyString;
+            }
+            set {
+                // Set your custom public key here
+                _keyString = "";
+            }
+        }
+
+        public static string FileDirectory {
+            get {
+                return Directory.GetCurrentDirectory() + @"\public_key.pem";
+            }
+        }
+    }
+}
