47e32ad09fccba85ccbe6349e75f9f5c928c4f07
Ahira <justiceahira@gmail.com>
2020-09-04 08:39:59 +0100

    Create LicenseKeyModel to represent the parts of a license key
    The four properties of LicenseKeyModel are the bare constituents of a
    license key. More might be added on later

---
+++ src/Licensing/Models/LicenseKeyModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Licensing.Models {
+
+    [Serializable]
+    public class LicenseKeyModel {
+
+        public string Signature { get; }
+        public string Data { get; }
+        public string ActivatedAt { get; set; }
+        public int Expiry { get; }
+
+        public LicenseKeyModel() { }
+
+        public LicenseKeyModel(string signature, string data, int expiry) {
+            Signature = signature;
+            Data = data;
+            Expiry = expiry;
+        }
+
+    }
+    
+}
